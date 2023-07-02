34cf93a22f1879ea01e7727d9c1ca9f10c660ab2
miraz-mondal <mirazmondal.official@gmail.com>
2023-07-02 22:52:43 +0530

    Project mapping done

---
+++ sanity/sanity-utils.ts
@@ -0,0 +1,21 @@
+import { createClient, groq } from "next-sanity";
+
+export async function getProjects() {
+  const client = createClient({
+    projectId: "emwximhy",
+    dataset: "production",
+    apiVersion: "2021-03-25",
+  });
+
+  return client.fetch(
+    groq`*[_type == "project"]{
+        _id,
+        _createdAt,
+        name,
+        "slug": slug.current,
+        "image": image.asset->url,
+        url,
+        content
+    }`
+  );
+}
