9ddd8029997dee3baff5d3730a1d04c6cab7fabe
miraz-mondal <mirazmondal.official@gmail.com>
2023-07-04 19:56:42 +0530

    Navbar title changed

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
             className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent"
           >
             {" "}
-            Miraz
+            Home
           </Link>
         </header>
         <main className="py-20">{children}</main>
