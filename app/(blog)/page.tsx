8cd7902747e528a07a94a6a20f2e2495f4377b46
miraz-mondal <mirazmondal.official@gmail.com>
2023-07-25 20:37:08 +0530

    mockup

--- app/(blog)/page.tsx
+++ app/(blog)/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
       </h1>
 
       <p className="mt-3 text-xl text-grey-600">Namaste, Check out my blog</p>
-
+      <p>about the movie blog</p>
       <h2 className="mt-24 font-bold text-gray-700">My Blogs</h2>
 
       <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
